eaabcb230437a7ce53604dc850deb2d22b27a6ed
djoudad292 <d.frih@esi-sba.dz>
2025-08-21 18:06:46 +0100

    .

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,23 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  tailwindcss: {
+    config: {
+      // Your Tailwind CSS configuration options
+      theme: {
+        extend: {
+          colors: {
+            // Custom colors
+            background: "#0D0D0D",
+            primary: "#6C63FF",
+            secondary: "#00E5FF",
+            text: "#FFFFFF",
+            border: "#A1A1AA"
+
+          },
+        },
+      },
+    },
+  },
 };
 
 export default config;
